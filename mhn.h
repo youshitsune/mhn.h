9e1166e915acdd03d2ed67b16c780cc87d8166f3
youshitsune <youshitsune@dmz.rs>
2025-10-15 19:03:45 +0200

    refactor: improve data manipulation

--- mhn.h
+++ mhn.h
@@ -1,98 +1,95 @@
-#include <stdlib.h>
 #include <math.h>
 
-#ifndef N
-#define N 2
-#endif
+#define to_pointer(arr) &arr[0][0]
 
-#ifndef P
-#define P 1
-#endif
+typedef struct{
+    int p;
+    int n;
+    float beta;
+    float *X;
+    float *eps;
+} Net;
 
-#ifndef BETA
-#define BETA 8.0f
-#endif
 
-void net_softmax(float arr[P][1]) {
+Net net_init(int p, int n, float beta, float *X, float *eps) {
+    Net net = {
+        .p = p,
+        .n = n,
+        .beta = beta,
+        .X = X,
+        .eps = eps,
+    };
+    
+    return net;
+}
+
+void net_softmax(float *arr, int p) {
     const float e = 2.71828;
 
-    float mx = arr[0][0];
-    for (int i = 1; i < P; i++) {
-        if (mx < arr[i][0]) mx = arr[i][0];
+    float mx = arr[0];
+    for (int i = 1; i < p; i++) {
+        if (mx < arr[i]) mx = arr[i];
     }
 
     float sum = 0;
-    for (int i = 0; i < P; i++) {
-        arr[i][0] = pow(e, arr[i][0]-mx);
-        sum += arr[i][0];
+    for (int i = 0; i < p; i++) {
+        arr[i] = pow(e, arr[i]-mx);
+        sum += arr[i];
     }
 
-    for (int i = 0; i < P; i++) {
-        arr[i][0] /= sum;
-    }
-}
-
-void net_scalarmul(float arr[P][1], float v) {
-    for (int i = 0; i < P; i++) {
-        arr[i][0] *= v;
+    for (int i = 0; i < p; i++) {
+        arr[i] /= sum;
     }
 }
 
-void net_transpose(float X[P][N], float T[N][P]) {
-    for (int i = 0; i < P; i++) {
-        for (int j = 0; j < N; j++) {
-            T[j][i] = X[i][j];
-        }
+void net_scalarmul(float *arr, int n, float v) {
+    for (int i = 0; i < n; i++) {
+        arr[i] *= v;
     }
 }
 
-void net_update_rule(float X[P][N], float T[N][P], float eps[N][1]){
-    float res[P][1];
+void net_update_rule(Net net){
+    float res[net.p];
 
     // matmul
-    for (int i = 0; i < P; i++) {
-        for (int j = 0; j < N; j++) {
-            res[i][0] += X[i][j]*eps[j][0];
+    for (int i = 0; i < net.p; i++) {
+        for (int j = 0; j < net.n; j++) {
+            res[i] += net.X[i*net.n+j]*net.eps[j];
         }
     }
 
-    net_scalarmul(res, BETA);
-    net_softmax(res);
+    net_scalarmul(res, net.p, net.beta);
+    net_softmax(res, net.p);
     
     //matmul
-    for (int i = 0; i < N; i++) {
-        eps[i][0] = 0;
-        for (int j = 0; j < P; j++) {
-            eps[i][0] += T[i][j]*res[j][0];
+    for (int i = 0; i < net.n; i++) {
+        net.eps[i] = 0;
+        for (int j = 0; j < net.p; j++) {
+            net.eps[i] += net.X[j*net.n+i]*res[j];
         }
     }
-
 }
 
-int net_equals(float arr1[N][1], float arr2[N][1]) {
-    for (int i = 0; i < N; i++) {
-        if (arr1[i][0] != arr2[i][0]) return 0;
+static inline int net_equals(float *arr1, float *arr2, int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr1[i] != arr2[i]) return 0;
     }
 
     return 1;
 }
 
-void net_copy(float arr1[N][1], float arr2[N][1]) {
-    for (int i = 0; i < N; i++) {
-        arr1[i][0] = arr2[i][0];
+static inline void net_copy(float *arr1, float *arr2, int n) {
+    for (int i = 0; i < n; i++) {
+        arr1[i] = arr2[i];
     }
 }
 
-void net_converge(float X[P][N], float eps[N][1]) {
-    float T[N][P];   
-    net_transpose(X, T);
-
-    float pre[N][1];
-    for (int i = 0; i < N; i++) pre[i][0] = 0;
+void net_converge(Net net) {
+    float pre[net.n];
+    for (int i = 0; i < net.n; i++) pre[i] = 0;
     
-
-    while (!net_equals(pre, eps)) {
-        net_copy(pre, eps);
-        net_update_rule(X, T, eps);
+    while (!net_equals(pre, net.eps, net.n)) {
+        net_copy(pre, net.eps, net.n);
+        net_update_rule(net);
     }
 }
